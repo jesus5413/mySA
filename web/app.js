ad0a0ba7fbbcf630b2e09175b69cf7cc0cd649dd
Fernando Renteria <rentfern@gmail.com>
2018-06-13 18:11:56 -0500

    Add anchors to lead to new pages for feed and item creation

--- web/app.js
+++ web/app.js
@@ -9,7 +9,15 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.post("/", (req, res) =>{
+app.get("/feeds", (req, res) => {
+    res.render("feeds");
+});
+
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+
+app.post("/", (req, res) => {
     res.render("index");
 });
 

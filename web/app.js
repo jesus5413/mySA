35984fce8067e497e7fefb5e6b767f5158378a86
Fernando Renteria <rentfern@gmail.com>
2018-06-16 14:19:23 -0500

    Added temporary authentication to implement feeds

--- web/app.js
+++ web/app.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const firebase = require("firebase");
+const path = require("path");
 
+// firebase configuration
+let config = {
+    apiKey: "AIzaSyDfSRGfcX-Xq-ynQENEeZtO2IjU9MN3WZM",
+    authDomain: "mysa-734b1.firebaseapp.com",
+    databaseURL: "https://mysa-734b1.firebaseio.com/",
+    storageBucket: "mysa-734b1.appspot.com"
+};
+
+firebase.initializeApp(config);
+let database = firebase.database(); // gets reference to database service
+
+//dummy log in for now
+authenticate("testmail2@gmail.com", "Password");
+
+function authenticate(email, password){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch((err) => {
+        // error
+        let errorCode = err.code;
+        let errorMessage = err.message;
+    
+        alert("Error");
+    });
+};
+
+firebase.auth().onAuthStateChanged((user) => {
+    if(user){
+        console.log("Signed in");
+    }
+});
+
+// Express route configuration
 let app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
 
-// routes
+// GET ROUTES
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -17,8 +51,13 @@ app.get("/create", (req, res) => {
     res.render("create");
 });
 
+// POST ROUTES
 app.post("/", (req, res) => {
     res.render("index");
+});
+
+app.post("/feeds", (req, res) => {
+    res.render("feeds");
 });
 
 // initialize server
---
+++ web/public/js/auth.js
@@ -0,0 +1,22 @@
+// const firebase = require("firebase");
+var firebaseui = require("firebaseui");
+
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// var uiConfig = {
+//     callbacks: {
+//         signInSuccessWithAuthResult: function(authResult, redirectUrl){
+//             return true;
+//         },
+//         uiShow: function() {
+//             document.getElementById("loader").style.display = "none";
+//         }
+//     },
+//     signIngFlow: "popup",
+//     signInSuccessUrl: "www.google.com",
+//     signInOptions: [
+//         firebase.auth.EmailAuthProvider.PROVIDER_ID 
+//     ]
+// };
+
+// ui.start("#firebaseui-auth-container", uiConfig);

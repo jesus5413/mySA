4acd2f493220f8a42aa59dad506e2904a8249667
jesus5413 <wwh124@my.utsa.edu>
2018-06-19 20:49:25 -0500

    View fragmentation completed

--- app/src/main/java/com/group/mysa/Controller/MainAppHomeActivity.java
+++ app/src/main/java/com/group/mysa/Controller/MainAppHomeActivity.java
@@ -3,30 +3,35 @@ package com.group.mysa.Controller;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.group.mysa.R;
 
 public class MainAppHomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText("Home");
+                    transaction.replace(R.id.content, new HomeFragment()).commit();
                     return true;
                 case R.id.navigation_city:
-                    mTextMessage.setText("City");
+                    transaction.replace(R.id.content, new CityFragment()).commit();
                     return true;
                 case R.id.navigation_profile:
-                    mTextMessage.setText("Profile");
+                    transaction.replace(R.id.content, new ProfileFragment()).commit();
                     return true;
             }
             return false;
@@ -38,7 +43,7 @@ public class MainAppHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

1d0bf69242f0bc7088b0cc1a8289c206e38d9998
Fernando Renteria <rentfern@gmail.com>
2018-06-21 11:18:36 -0500

    Clean up app.js a bit

--- web/app.js
+++ web/app.js
@@ -1,3 +1,4 @@
+// require statements
 require("dotenv").config();
 const express = require("express");
 const firebase = require("firebase");
@@ -5,8 +6,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 // const databaseModule = require("./public/js/database.js");
 
+// globabl variables
+var database;
+var feedRef;
+
 // firebase configuration
-let config = {
+var config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
@@ -31,6 +36,8 @@ function authenticate(email, password){
 firebase.auth().onAuthStateChanged((user) => {
     if(user){
         console.log("Signed in");
+        database = firebase.database();
+        feedRef = database.ref("feed");
     }
 });
 
@@ -41,19 +48,20 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// functions
+function getFeedAndRender(res){
+    feedRef.once("value", (snapshot) => {
+        res.render("feeds", { posts: snapshot });
+    });
+}
+
 // GET ROUTES
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/feeds", (req, res) => {
-    // retrieve all items from db
-    let database = firebase.database();
-    let feedRef = database.ref("feed");
-    
-    feedRef.once("value", (snapshot) => {
-        res.render("feeds", { posts: snapshot });
-    });
+    getFeedAndRender(res);
 });
 
 // POST ROUTES
@@ -63,8 +71,6 @@ app.post("/", (req, res) => {
 
 // This will add new entry into the database 
 app.post("/feeds", (req, res) => {
-    let database = firebase.database(); // gets reference to database service
-    let feedRef = database.ref("feed"); // gets reference to the feed field of database
     let newItem = feedRef.push(); // pushes EMPTY record to database
 
     // this will actually write out all of the required items to that record
@@ -77,11 +83,7 @@ app.post("/feeds", (req, res) => {
     });
 
     console.log("added to db");
-
-    // retrieve all items from db
-    feedRef.once("value", (snapshot) => {
-        res.render("feeds", { posts: snapshot });
-    });
+    getFeedAndRender(res);
 });
 
 // initialize server
